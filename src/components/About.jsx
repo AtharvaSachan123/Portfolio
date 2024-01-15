c4db5679649db1a87cfce0efd2f2b05c43d08b58
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-01-15 23:56:54 +0530

    Portfolio section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,17 +5,15 @@ const About = () => {
     <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
       
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
+        <div className='pt-8'>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
         
             <p className='text-xl mt-20 '>
-            I am a dedicated student pursuing a B.Tech degree at KIET Group of Institutions in Ghaziabad, focusing on Information Technology. Enthusiastic about staying at the forefront of technological advancements, I have a keen interest in learning and exploring new concepts. As a proficient frontend developer, my expertise lies in working with React JS, where I constantly strive to create engaging and user-friendly interfaces. I am driven by a passion for innovation and am currently open to internship opportunities that allow me to contribute to real-world projects and continue expanding my skill set.
-            </p>
+            A Dedicated B.Tech student at KIET, Ghaziabad, specializing in Information Technology. Proficient in React JS, I excel in crafting captivating and user-friendly interfaces. Eager to stay abreast of tech trends, I seek internship opportunities to apply my skills, contribute to real-world projects, and fuel my passion for innovation.            </p>
             <br/>
             <p className='text-xl'>
-            My journey as a student has provided me with a strong foundation in IT, and my experience as a frontend developer has honed my abilities in creating dynamic and responsive web applications. Being adaptable and open to embracing new challenges, I am eager to apply my knowledge and skills in a professional setting through an internship. I believe in the power of continuous learning, and I am ready to contribute my enthusiasm and dedication to a team that values innovation and excellence in the field of Information Technology.
-            </p>
+            As an IT student, I've built a robust foundation, and my frontend development experience has sharpened my skills in crafting dynamic web applications. Adaptable and open to challenges, I'm eager for a professional internship to apply my knowledge. Committed to continuous learning, I'm ready to contribute enthusiasm and dedication to an innovative IT team.            </p>
         </div>
       </div>
 
