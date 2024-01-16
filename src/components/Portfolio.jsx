8d1a00dc2ebc1388e21b3a5ab2099252bd593d07
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-01-16 23:39:02 +0530

    Portfolio Section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,32 +11,47 @@ const Portfolio = () => {
     {
       id: 1,
       src: arrayDestruct,
-      demoLink: "https://www.google.com",
+      demoLink: "https://crypto-hunt1.vercel.app/",
+      demoCode:"https://github.com/AtharvaSachan123/Crypto-Hunt.git",
     },
     {
       id: 2,
       src: reactParallax,
+      demoLink:"https://atharva1.vercel.app/",
+      demoCode:"https://github.com/AtharvaSachan123/Portfolio",
     },
     {
       id: 3,
       src: navbar,
+      demoLink:"https://calculator-theta-eight-15.vercel.app/",
+      demoCode:"https://github.com/AtharvaSachan123/calculator",
     },
     {
       id: 4,
       src: reactSmooth,
+      demoLink:"https://moveitwebsite.vercel.app/",
+      demoCode:"https://github.com/AtharvaSachan123/html-moveitwebsite",
     },
     {
       id: 5,
       src: installNode,
+      demoLink:"https://temperature-converter-pied.vercel.app/",
+      demoCode:"https://github.com/AtharvaSachan123/temperature-converter",
+
     },
     {
       id: 6,
       src: reactWeather,
+      demoLink:"https://weather-app-nine-self.vercel.app/",
+      demoCode:"https://github.com/AtharvaSachan123/weather-app",
     },
   ];
 
   const handleDemoButtonClick = (demoLink) => {
     window.location.href = demoLink;
+  };
+  const handleCodeButtonClick = (demoCode) => {
+    window.location.href = demoCode;
   };
 
   return (
@@ -53,7 +68,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src, demoLink }) => (
+          {portfolios.map(({ id, src, demoLink,demoCode}) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -63,11 +78,13 @@ const Portfolio = () => {
               <div className="flex items-center justify-center">
                 <button
                   onClick={() => handleDemoButtonClick(demoLink)}
-                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-blue-400"
                 >
                   Demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button 
+                onClick={()=> handleCodeButtonClick(demoCode)}
+                className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:text-blue-400">
                   Code
                 </button>
               </div>
