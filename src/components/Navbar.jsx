b8e3a8ff8706137a609f118b58e928c57b4dd1f6
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-01-18 00:02:19 +0530

    Final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {FaBars,FaTimes} from 'react-icons/fa'
+import {Link} from 'react-scroll'
 const Navbar = () => {
 const [nav,setNav]=useState(false);
   const links =[
@@ -35,7 +36,7 @@ const [nav,setNav]=useState(false);
               <li
                key={id}
                className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200 hover:text-blue-400'>
-                {link}
+                <Link to={link} smooth duration={500}>{link}</Link>
                </li>
 
       ))}
@@ -49,7 +50,8 @@ const [nav,setNav]=useState(false);
                  <li
                   key={id}
                   className='px-4 cursor-pointer capitalize py-6 text-4xl'>
-                   {link}
+                                   <Link onClick={()=>setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
+
                   </li>
    
          ))}        
